3c8f8a2deaabb547f969e6ea75015b23fd7be2e1
mgjeffries <gib.jeffries@gmail.com>
2020-12-17 17:04:55 -0600

    remove notion of admin

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -45,9 +45,6 @@ export const Register = (props) => {
         // and redirect us on to the app with those values set
         localStorage.setItem("whoyou_user_token", res.token);
         localStorage.setItem("whoyou_user_id", res.user_id);
-        if (res.is_admin) {
-          localStorage.setItem("is_admin", "true");
-        }
         props.history.push("/");
       });
   };
