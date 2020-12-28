8cc1c94a7135ef119f1bbe8ec5951221a2994efb
mgjeffries <gib.jeffries@gmail.com>
2020-12-28 11:20:18 -0500

    add user provider

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { UserProvider } from "./components/user/UserProvider";
 
 export const ApplicationViews = () => {
   return (
     <>
-      <Route exact path="/">
-        <div>Yo</div>
-      </Route>
-      <Route exact path="/users"></Route>
+      <UserProvider>
+        <Route exact path="/">
+          <div>Yo</div>
+        </Route>
+
+        <Route exact path="/users"></Route>
+      </UserProvider>
     </>
   );
 };
---
+++ src/components/user/UserProvider.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export const UserContext = React.createContext();
+
+export const UserProvider = (props) => {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = () => {
+    return fetch("http://localhost:8000/users")
+      .then((res) => res.json())
+      .then(setUsers);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        users,
+        getUsers,
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
+};
