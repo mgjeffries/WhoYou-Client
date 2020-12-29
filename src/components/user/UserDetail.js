6484bd81354797df87d65ec87704f1fec88574ab
mgjeffries <gib.jeffries@gmail.com>
2020-12-29 17:06:41 -0500

    only match requests authored by the user

--- src/components/user/UserDetail.js
+++ src/components/user/UserDetail.js
@@ -21,21 +21,22 @@ export const UserDetail = (props) => {
       {userContent.map((content) => {
         const matchingRequest = contentViewRequests.find((viewRequest) => {
           return (
+            // find a request that was created by this user AND
+            viewRequest.requester.id === currentUser &&
+            // the requested content matches the current
             viewRequest.content.id === content.id
-            // &&
-            // viewRequest.requester.id === currentUser
           );
         });
 
         return (
-          <>
-            <div key={content.id}>
+          <div key={content.id}>
+            <div>
               {content.field_type.name}: {content.value}
             </div>
             <div>
               Is there a matching request: {matchingRequest ? "yes" : "no"}
             </div>
-          </>
+          </div>
         );
       })}
     </>
