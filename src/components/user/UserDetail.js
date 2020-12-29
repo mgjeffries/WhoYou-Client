1bad0e9ec130820fd235b6c31f894edb45c83585
mgjeffries <gib.jeffries@gmail.com>
2020-12-29 12:39:06 -0500

    Add user content length to user detail

--- src/components/user/UserDetail.js
+++ src/components/user/UserDetail.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { GetContentByUserId } from "../content/GetContentByUserId.js";
 
 export const UserDetail = (props) => {
-  const currentUser = parseInt(localStorage.getItem("whoyou_user_id"));
+  // const currentUser = parseInt(localStorage.getItem("whoyou_user_id"));
   const [userContent, setUserContent] = useState({});
   const { userId } = useParams();
 
   useEffect(() => {
-    // getPostById(postId).then(setPost);
+    const userContentResponse = GetContentByUserId(userId).then(setUserContent);
   }, []);
 
-  return <div>User Detail for user {userId}</div>;
+  return (
+    <>
+      <div>User Detail for user {userId}</div>
+      <div>Content Count {userContent.length}</div>
+    </>
+  );
 };
