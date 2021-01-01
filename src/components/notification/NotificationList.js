9c4c1b4734f93b3937fd1bfa1a6ce6da0e851dbc
mgjeffries <gib.jeffries@gmail.com>
2021-01-01 16:17:50 -0500

    get content view requests by other users in notification component state

--- src/components/notification/NotificationList.js
+++ src/components/notification/NotificationList.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ContentViewRequestContext } from "../content_view_request/ContentViewRequestProvider";
 
 export const NotificationList = () => {
-  return <div>Notifications</div>;
+  const currentUser = parseInt(localStorage.getItem("whoyou_user_id"));
+  const { contentViewRequests, getContentViewRequests } = useContext(
+    ContentViewRequestContext
+  );
+  const [
+    contentViewRequestsByOthers,
+    setContentViewRequestsByOthers,
+  ] = useState([]);
+
+  useEffect(() => {
+    getContentViewRequests();
+  }, []);
+
+  useEffect(() => {
+    const updatedContentViewRequestsByOthers = contentViewRequests.filter(
+      (contentViewRequest) => {
+        return contentViewRequest.requester.id !== currentUser;
+      }
+    );
+    setContentViewRequestsByOthers(updatedContentViewRequestsByOthers);
+  }, contentViewRequests);
+
+  return (
+    <div>
+      {contentViewRequestsByOthers.map((contentViewRequest) => {
+        return contentViewRequest.requester.name;
+      })}
+    </div>
+  );
 };
